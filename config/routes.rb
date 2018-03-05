80c2e3e434e64559e6f23205318198f78d6c756c
Ah Hyeon Kim <sepo_o@daum.net>
2018-03-05 05:55:36 +0000

    making controller and view and then specifying route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'email/email_form' => 'email#email_form'
+ 	get 'email/emial_send' => 'email#email_send'
 end
