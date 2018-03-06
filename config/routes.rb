ac226658ed37b809b88a427e82db4c6698c2eaa4
Ah Hyeon Kim <sepo_o@daum.net>
2018-03-06 16:26:01 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	get 'email/email_form' => 'email#email_form'
- 	get 'email/emial_send' => 'email#email_send'
+ 	get 'email/email_send' => 'email#email_send'
 end
